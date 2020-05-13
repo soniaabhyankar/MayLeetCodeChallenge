92baa05fd9ccf4e979ff68f1014bcf97d28ff125
Sonia Abhyankar <sonia.abhyankar@gmail.com>
2020-05-13 23:34:00 +0200

    Day 12

---
+++ 12_single_element_in_sorted_array.py
@@ -0,0 +1,25 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        
+        left = 0
+        right = len(nums) - 1
+        
+        while left < right:
+            mid = (left + right) // 2
+            
+            check_if_halves_even = (right - mid) % 2 == 0
+            
+            if nums[mid+1] == nums[mid]:
+                if check_if_halves_even:
+                    left = mid + 2
+                else:
+                    right = mid - 1
+            elif nums[mid-1] == nums[mid]:
+                if check_if_halves_even:
+                    right = mid - 2
+                else:
+                    left = mid + 1
+            else:
+                return nums[mid]
+            
+        return nums[left]
